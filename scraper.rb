40683502f77577ad57ca25b41c90b0fffe5e709a
Oliver Denman <oliver@therewillbeanimals.com>
2016-12-02 10:59:47 +0000

    Tidy scraper.rb for Rubocop
    Fixes indentation, removes explicit `self` and removes hanging commas.

--- scraper.rb
+++ scraper.rb
@@ -16,7 +16,7 @@ require_rel 'lib'
 
 class String
   def tidy
-    self.gsub(/[[:space:]]+/, ' ').strip
+    gsub(/[[:space:]]+/, ' ').strip
   end
 end
 
@@ -30,7 +30,7 @@ terms = (2015..Date.today.year).map do |year|
   {
     id: year,
     start_date: Date.new(year, 3, 1).to_s,
-    end_date: Date.new(year, 11, 30).to_s,
+    end_date: Date.new(year, 11, 30).to_s
   }
 end
 
@@ -39,9 +39,9 @@ def scrape_list(url)
              .members
              .map(&:to_h)
              .map do |member|
-              member.merge(MemberPage.new(
-                response: Scraped::Request.new(url: member[:source]).response
-                ).to_h)
+               member.merge(MemberPage.new(
+                 response: Scraped::Request.new(url: member[:source]).response
+               ).to_h)
              end
 end
 
@@ -49,7 +49,7 @@ memberships_from_page = scrape_list('http://www.senado.gov.ar/senadores/listados
 
 data = CombinePopoloMemberships.combine(
   id: memberships_from_page,
-  term: terms,
+  term: terms
 )
 
 ScraperWiki.save_sqlite([:id, :term], data)
