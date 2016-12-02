640170cc27fee6f67711ceb4453faddb3d18e92b
Oliver Denman <oliver@therewillbeanimals.com>
2016-12-02 10:27:03 +0000

    Add MemberPage
    This class represents the member page of an individual member.

---
+++ lib/member_page.rb
@@ -0,0 +1,21 @@
+require 'scraped'
+
+class MemberPage < Scraped::HTML
+  field :name do
+    "#{given_name} #{family_name}"
+  end
+
+  field :given_name do
+    member_info.at_xpath('./text()').text.tidy
+  end
+
+  field :family_name do
+    member_info.at_xpath('./span').text.tidy
+  end
+
+  private
+
+  def member_info
+    noko.at_xpath('//div[@class="col-sm-12 col-md-5"]/div')
+  end
+end
