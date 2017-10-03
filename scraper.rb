361653b3f34c7168d6f6e2c8d0446a1d688368e7
Tony Bowden <tony@tmtm.com>
2017-10-03 15:00:16 +0100

    Show debug output if MORPH_DEBUG is set

--- scraper.rb
+++ scraper.rb
@@ -54,6 +54,7 @@ data = CombinePopoloMemberships.combine(
   id: memberships_from_page,
   term: terms
 )
+data.each { |mem| puts mem.reject { |_, v| v.to_s.empty? }.sort_by { |k, _| k }.to_h } if ENV['MORPH_DEBUG']
 
 ScraperWiki.sqliteexecute('DROP TABLE data') rescue nil
 ScraperWiki.save_sqlite(%i[id term], data)
